64985664238de185a9f1b6c4fc680f5547e0af53
Jay Hack <jhack@stanford.edu>
2015-09-10 09:46:18 -0700

    make_figure now works

--- server.py
+++ server.py
@@ -16,27 +16,48 @@ app = flask.Flask(__name__)
 dbclient = DBClient()
 
 
-@app.route("/")
-def plot():
-    """Grabs all active models and plots them"""
-    args = flask.request.args
-    print args
-
-    dbclient = DBClient()
-    
-    model_name = 'my_first_model'
+def make_figure(model_name):
+    """makes and returns a figure"""
     mt = dbclient.get_model_tracker(model_name)
     loss_records = mt.get_records('loss')
     acc_records = mt.get_records('accuracy')
-
     fig = figure(
-                # title='{}'.format(model_name),
                 tools='pan,wheel_zoom,reset',
                 plot_width=500,
                 plot_height=300
-                )
-    fig.line(range(len(loss_records)), loss_records['data'])
-    fig.line(range(len(acc_records)), acc_records['data'])
+            )
+
+    # TODO: switch this to timestamp?
+    x = range(len(loss_records))
+    fig.circle(x, loss_records['data'], legend="loss", color="red")
+    fig.line(x, loss_records['data'], legend="loss", color="red")
+    fig.circle(x, acc_records['data'], legend="accuracy", color="green")
+    fig.line(x, acc_records['data'], legend="accuracy", color="green")
+
+    return fig
+
+
+@app.route("/")
+def plot():
+    """Grabs all active models and plots them"""
+    
+
+
+    # model_name = 'my_first_model'
+    # mt = dbclient.get_model_tracker(model_name)
+    # loss_records = mt.get_records('loss')
+    # acc_records = mt.get_records('accuracy')
+
+    # fig = figure(
+    #             # title='{}'.format(model_name),
+    #             tools='pan,wheel_zoom,reset',
+    #             plot_width=500,
+    #             plot_height=300
+    #             )
+    # fig.line(range(len(loss_records)), loss_records['data'])
+    # fig.line(range(len(acc_records)), acc_records['data'])
+
+    fig = make_figure('quadratic_model')
 
     # Configure resources to include BokehJS inline in the document.
     # For more details see:
